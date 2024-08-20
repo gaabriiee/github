3a2d79cb19e9385d8417f20b546c4e201d20f315
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:55:29 -0300

    adicionada função para subtrair itens (números) em um array
    a função subtrairNumerosArray recebe um array de números e retorna a soma de todos os itens do array

--- calculos/basicos-subtracao.js
+++ calculos/basicos-subtracao.js
@@ -6,3 +6,12 @@
 export function subtrairNumeros(...numeros) {
   return numeros.reduce((total, proximo) => total - proximo, 0);
 }
+
+/**
+ * Subtrai valores numéricos presentes em um array
+ * @param {number} numeros Valores a serem subtraídos
+ * @returns Total da subtração
+ */
+export function subtrairNumerosArray(numeros) {
+  return numeros.reduce((total, proximo) => total - proximo, 0);
+}
