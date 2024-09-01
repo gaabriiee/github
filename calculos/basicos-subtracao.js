4619a202d9c32dffff2d741d1bafd4c9dc8142d5
gaabriiee <140844033+gaabriiee@users.noreply.github.com>
2024-09-01 20:19:23 -0300

    adicionada função para subtrair strings
    a função subtrairStrings pode receber um número arbitrário de parâmetros e retorna o valor da subtração desses parâmetros (após conversão para números)

--- calculos/basicos-subtracao.js
+++ calculos/basicos-subtracao.js
@@ -7,3 +7,13 @@ export function subtrairStringsArray(numeros) {
     const valores = numeros.map((n) => Number(n));
     return subtrairNumerosArray(valores);
   }
+
+/**
+ * Subtrai, como valores numéricos, valores em strings
+ * @param  {...number} numeros Valores a serem subtraídos
+ * @returns Total da subtração
+ */
+export function subtrairStrings(...numeros) {
+    const valores = numeros.map((n) => Number(n));
+    return subtrairNumerosArray(valores);
+  }
