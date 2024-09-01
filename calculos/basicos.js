ffa13ca6c06bfe87efe26af6c56f94996d207c5f
gaabriiee <140844033+gaabriiee@users.noreply.github.com>
2024-09-01 20:22:40 -0300

    bug corrigido

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -1,1 +1,9 @@
-// AQUI SERÃO IMPLEMENTADAS AS FUNÇÕES BÁSICAS DE CALCULO
+/**
+ * Soma, como valores numéricos, valores em strings
+ * @param  {...string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStrings(...numeros) {
+    const valores = numeros.map((n) => Number(n));
+    return somarNumerosArray(valores);
+  }
